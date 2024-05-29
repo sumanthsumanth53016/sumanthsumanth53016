3831b15eb010c5cf7f91c31dd4dbe77cfb0e3597
sumanthsumanth53016 <msubrahmanyamswami@gmail.com>
2024-05-29 11:19:30 +0530

    Add files via upload

---
+++ haribillamount.cpp
@@ -0,0 +1,18 @@
+//program to calculate amount of hari
+
+#include <stdio.h>
+main()
+{
+
+	int a,b,total;
+	a=b=total=0;
+	printf("enter the price of 1kg rice: ");
+	scanf("%d",&a);
+	printf("enter the quantity of rice :  ");
+	scanf("%d",&b);
+	total=a*b*(1);
+	printf("total is: %d",total);
+	//new line
+	
+}
+
